e4b26837474008f5273ec0d4eb99118b751cb47a
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-06-01 09:22:01 +0530

    edit config for add custom image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    
+    images : {
+        domains : ['devscard.netlify.app', 'localhost', 'picsum.photos'] // <== Domain name
+      }
+};
+
+
 
 export default nextConfig;
